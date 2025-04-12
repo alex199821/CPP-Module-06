953bbd7447e088fbd91e8a5430ecd8667cf0d3f7
macbook <macbook@macbooks-MacBook-Pro.local>
2025-04-12 15:46:48 +0200

    ex00 first test

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,14 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ScalarConverter.cpp                               :+:      :+:    :+:   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: auplisas <auplisas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 04:36:19 by auplisas          #+#    #+#             */
-/*   Updated: 2025/04/12 04:36:37 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/04/12 14:43:45 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+void ScalarConverter::convert(const std::string &input)
+{
+	// if (!input.empty() && input.back() == 'f')
+	// {
+	// 	input.pop_back();
+	// }
+
+	try
+	{
+		float value = std::stof(input);
+		std::cout << "Converted float: " << value << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Conversion failed: " << e.what() << std::endl;
+	}
+}
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,34 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ScalarConverter.hpp                               :+:      :+:    :+:   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: auplisas <auplisas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 04:25:46 by auplisas          #+#    #+#             */
-/*   Updated: 2025/04/12 04:38:43 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/04/12 14:33:56 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
-#include <string>
-#include <limits>
-#include <cstdlib>
-#include <sstream>
 #include <cctype>
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <sstream>
+#include <string>
 
-class  ScalarConverter
+class ScalarConverter
 {
   private:
-
   public:
 	// Constructors
-    ScalarConverter() = delete;
-    ScalarConverter(const  ScalarConverter &copy) = delete;
+	ScalarConverter() = delete;
+	ScalarConverter(const ScalarConverter &copy) = delete;
 	~ScalarConverter() = delete;
-    ScalarConverter &operator=(const  ScalarConverter &copy) = delete;
+	ScalarConverter &operator=(const ScalarConverter &copy) = delete;
 	static void convert(const std::string &input);
 };
