09e79ae3943c295caa0888c076337bcb87808655
macbook <macbook@macbooks-MacBook-Pro.local>
2025-04-13 04:06:38 +0200

    add ex00 complete

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,42 +6,72 @@
 /*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 04:36:19 by auplisas          #+#    #+#             */
-/*   Updated: 2025/04/13 03:44:23 by macbook          ###   ########.fr       */
+/*   Updated: 2025/04/13 03:54:31 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-void	convertToChar(double value)
+bool	checkSpecialCases(const std::string &input)
 {
-	std::cout << "char: ";
-	if (std::isnan(value) || value < 0 || value > 127)
-		std::cout << "impossible" << std::endl;
-	else if (!std::isprint(static_cast<char>(value)))
-		std::cout << "Non displayable" << std::endl;
-	else
-		std::cout << "'" << static_cast<char>(value) << "'" << std::endl;
+	if (input == "nan" || input == "nanf")
+		return (true);
+	if (input == "+inf" || input == "+inff")
+		return (true);
+	if (input == "-inf" || input == "-inff")
+		return (true);
+	return (false);
+}
+
+void	handleSpecialCases(const std::string &input)
+{
+	if (input == "nan" || input == "nanf")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	else if (input == "+inf" || input == "+inff")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+	}
+	else if (input == "-inf" || input == "-inff")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+	}
 }
 
 void ScalarConverter::convert(const std::string &input)
 {
-	literal_type inputType;
-
-	inputType = selectType(input);
-
-	if (inputType == CHAR)
-		handleChar(input);
-	else if (inputType == INT)
-		handleInt(input);
-	else if (inputType == FLOAT)
-		handleFloat(input);
-	else if (inputType == DOUBLE)
-		handleDouble(input);
+	if (checkSpecialCases(input))
+		handleSpecialCases(input);
 	else
 	{
-		std::cout << "char: impossible\n";
-		std::cout << "int: impossible\n";
-		std::cout << "float: impossible\n";
-		std::cout << "double: impossible\n";
+		literal_type inputType;
+
+		inputType = selectType(input);
+
+		if (inputType == CHAR)
+			handleChar(input);
+		else if (inputType == INT)
+			handleInt(input);
+		else if (inputType == FLOAT)
+			handleFloat(input);
+		else if (inputType == DOUBLE)
+			handleDouble(input);
+		else
+		{
+			std::cout << "char: impossible\n";
+			std::cout << "int: impossible\n";
+			std::cout << "float: impossible\n";
+			std::cout << "double: impossible\n";
+		}
 	}
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,14 +6,20 @@
 /*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 14:43:49 by macbook           #+#    #+#             */
-/*   Updated: 2025/04/13 03:01:19 by macbook          ###   ########.fr       */
+/*   Updated: 2025/04/13 03:56:16 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-    ScalarConverter::convert("42.23f");
-	return (0);
+    if (argc == 2)
+	{
+		ScalarConverter::convert(argv[1]);
+		return (0);
+	}
+	else
+		std::cout << "Incorrect Number of Arguments." << std::endl;
+	return (1);
 }
